b8b0fbf268e4e424a1551ad89d5d7848d286f49d
Abdiel Wong <amwa_24@hotmail.com>
2024-11-25 15:17:27 -0500

    PlayerStateMachine Started

---
+++ Assets/Scripts/PlayerStateMachine/PlayerIdleState.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerIdleState : PlayerBaseState
+{
+    public PlayerIdleState(PlayerStateManager manager, PlayerData playerData)
+        : base(manager, playerData)
+    {
+    }
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+}
